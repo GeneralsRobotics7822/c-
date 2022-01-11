7dd1bf201f1c2bdf487a66ff761f5ad319548cd9
MenacingMango <97459048+MenacingMango@users.noreply.github.com>
2022-01-11 10:11:37 -0500

    Add files via upload

--- Robot/src/main/cpp/Robot.cpp
+++ Robot/src/main/cpp/Robot.cpp
@@ -2,22 +2,66 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-#include "Robot.h"
+#include <frc/XboxController.h>
+#include <frc/TimedRobot.h>
+#include <frc/Timer.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/motorcontrol/PWMSparkMax.h>
 
-void Robot::RobotInit() {}
-void Robot::RobotPeriodic() {}
+class Robot : public frc::TimedRobot {
+ public:
+  
+  bool going = true;
 
-void Robot::AutonomousInit() {}
-void Robot::AutonomousPeriodic() {}
+  void RobotInit() override{
+    m_right.SetInverted(true);
+  }
+  void RobotPeriodic() override{}
 
-void Robot::TeleopInit() {}
-void Robot::TeleopPeriodic() {}
+  void AutonomousInit() override{
+    m_timer.Reset();
+    m_timer.Start();
+  }
+  void AutonomousPeriodic() override{
+    if(m_timer.Get() < 2_s){
+      m_robotDrive.ArcadeDrive(0.5,0);
+    }
+    else{
+      m_robotDrive.StopMotor();
+    }
+  }
 
-void Robot::DisabledInit() {}
-void Robot::DisabledPeriodic() {}
+  void TeleopInit() override{}
+  void TeleopPeriodic() override{
+    if(going){
+      m_robotDrive.ArcadeDrive(-m_stick.GetRightX(), m_stick.GetRightY());
+    }
+    if(m_stick.GetBButtonPressed()){
+      m_robotDrive.StopMotor();
+      going = false;
+    }
+    if(m_stick.GetAButtonPressed()){
+      going = true;
+    }
+  }
 
-void Robot::TestInit() {}
-void Robot::TestPeriodic() {}
+  void DisabledInit() override{}
+  void DisabledPeriodic() override{}
+
+  void TestInit() override{}
+  void TestPeriodic() override{}
+
+  private:
+    // Robot drive system
+    frc::PWMSparkMax m_left{0};
+    frc::PWMSparkMax m_right{1};
+    frc::DifferentialDrive m_robotDrive{m_left, m_right};
+
+    frc::XboxController m_stick{0};
+    frc::Timer m_timer;
+
+};
+
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
